1eebf8a6fdabcc61f501dea1c1d26c0c71f12fe3
Dan Axå <dan.axa@nepa.com>
2017-08-01 14:22:51 +0200

    ==

---
+++ jbrains_tdd_intro/tests/FractionSpec.cs
@@ -0,0 +1,24 @@
+﻿using Cone;
+using under_test;
+
+namespace jbrains_tdd_intro.tests
+{
+    [Describe(typeof(under_test.Fraction))]
+    public class FractionSpec
+    {
+        public void to_float()
+        {
+            Check.That(() => F(1, 2).ToFloat() == 0.5);
+        }
+
+        public void supports_equality()
+        {
+            Check.That(() => F(1, 2) == F(1, 2));
+        }
+
+        private Fraction F(int nunerator, int denominator)
+        {
+            return Fraction.Create(nunerator, denominator);
+        }
+    }
+}
--- jbrains_tdd_intro/under_test/Fraction.fs
+++ jbrains_tdd_intro/under_test/Fraction.fs
@@ -8,3 +8,7 @@
 
         member this.ToFloat() = 
             (float)this.Numerator / (float)this.Denominator
+
+        static member op_Equality ((a: Fraction), (b: Fraction)) = a.Equals(b)
+        static member op_Inequality ((a: Fraction), (b: Fraction)) = a.Equals(b) = false            
+
