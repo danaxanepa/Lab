ba252db0f2d743b8cf60e01bf72e04867530bfa3
Dan Axå <dan.axa@nepa.com>
2017-08-03 19:49:09 +0200

    Initial commit

---
+++ jbrains_tdd_intro/tests/pos/OnBarCodeFeature.cs
@@ -0,0 +1,30 @@
+﻿using Cone;
+using under_test;
+
+namespace jbrains_tdd_intro.tests.pos
+{
+    [Feature("OnBarCode")]
+    public class OnBarCodeFeature
+    {
+        public void null_gives_invalid_barcode()
+        {
+            var fakeDisplay = new FakeDisplay();
+            System(fakeDisplay).OnBarCode(BarCode.Create(null));
+            Check.That(() => fakeDisplay.LastMessage == "Invalid barcode");
+        }
+
+        private under_test.PointOfSaleSystem System(Display display)
+        {
+            return new PointOfSaleSystem(display);
+        }
+
+        private class FakeDisplay : Display
+        {
+            public string LastMessage;
+            public void Print(string message)
+            {
+                LastMessage = message;
+            }
+        }    
+    }
+}
---
+++ jbrains_tdd_intro/under_test/pos.fs
@@ -0,0 +1,13 @@
+﻿namespace under_test
+
+    type BarCode = 
+        { Value: string }
+        static member Create v = { Value = v }
+
+    type Display = 
+        abstract member Print : string -> unit
+
+    type PointOfSaleSystem (display: Display) =
+        
+        member this.OnBarCode (value: BarCode) = 
+            display.Print "Invalid barcode"
