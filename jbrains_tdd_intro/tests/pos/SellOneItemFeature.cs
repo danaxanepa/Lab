0b655bd0f3cf21ea7d4b71c8378342732380d18b
Dan Axå <dan.axa@nepa.com>
2017-08-04 21:04:06 +0200

    Renamed specs

--- jbrains_tdd_intro/tests/pos/SellOneItemFeature.cs
+++ jbrains_tdd_intro/tests/pos/SellOneItemFeature.cs
@@ -1,27 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Cone;
 using under_test;
 
 namespace jbrains_tdd_intro.tests.pos
 {
-    [Describe(typeof(BarCode))]
-    public class BarCodeSpec
-    {
-        public void does_not_accept_null()
-        {
-            Check.Exception<ArgumentException>(() => BarCode.Create(null));
-        }
-
-        public void does_not_accept_empty()
-        {
-            Check.Exception<ArgumentException>(() => BarCode.Create(string.Empty));
-        }
-    }
-
-    [Feature("OnBarCode")]
-    public class OnBarCodeFeature
+    [Feature("SellOneItem")]
+    public class SellOneItemFeature
     {
         public void handle_unknown_barcode()
         {
